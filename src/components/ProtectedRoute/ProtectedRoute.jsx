0e7f2594d487e38eefb129c8b4fd942d85af748e
andresfrei <andresfrei@gmail.com>
2023-07-05 18:41:48 -0300

    Protected routes

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,9 +1,7 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import useSession from '../../hooks/useSession'
 
-const ProtectedRoute = ({ children, redirectTo = '/' }) => {
-  const { session } = useSession()
-  return session.status !== 2 ? <Navigate to={redirectTo} /> : (children || <Outlet />)
+export default function ProtectedRoute ({ children, redirectTo = '/' }) {
+  const { hasLogged } = useSession()
+  return !hasLogged ? <Navigate to={redirectTo} /> : (children || <Outlet />)
 }
-
-export default ProtectedRoute
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -5,22 +5,36 @@ import LayoutPage from '../layouts/LayoutPage'
 import Home from '../pages/Home'
 import About from '../pages/About'
 import Detail from '../pages/Detail'
-import Error from '../pages/Error'
 import Landing from '../pages/Landing'
+import ProtectedRoute from '../components/ProtectedRoute/ProtectedRoute'
+
+import Error from '../pages/Error'
 
 const router = createBrowserRouter([
-  { path: '/about', element: <About/> },
+
   {
     path: '/',
     element: <LayoutPage/>,
-    errorElement: <Error/>,
     children: [
-      { path: '/', element: <Landing /> },
-      { path: '/home', element: <Home /> },
-      { path: '/detail:id', element: <Detail/> }
+      {
+        path: '/',
+        element: <Landing/>
+      },
+      {
+        path: '/about',
+        element: <About/>
+      },
+      {
+        path: '/',
+        element: <ProtectedRoute/>,
+        errorElement: <Error/>,
+        children: [
+          { path: '/home', element: <Home /> },
+          { path: '/detail:id', element: <Detail/> }
+        ]
+      }
     ]
   }
-
 ])
 
 export default router
